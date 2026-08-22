62e9bb93f19b66b247f09a5d9c3629b035962627
Joshan Kana <127849942+joshan-kana@users.noreply.github.com>
2026-08-23 02:58:59 +0800

    refactor: restore lint tooling and aliases

--- flake.nix
+++ flake.nix
@@ -45,12 +45,44 @@
       };
 
       treefmtFor = system: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} treefmtConfig;
+
+      lintFor =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.writeShellApplication {
+          name = "lint";
+          runtimeInputs = [
+            pkgs.shellcheck
+            pkgs.statix
+          ];
+          text = ''
+            statix check flake.nix
+            statix check devenv.nix
+            shellcheck -s bash .envrc
+          '';
+        };
+
+      runCheck =
+        system: pkg:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.runCommand "isys2014-${pkg.name}-check" { nativeBuildInputs = [ pkg ]; } ''
+          cp -R ${self} source
+          chmod -R u+w source
+          cd source
+          ${pkg}/bin/${pkg.name}
+          touch "$out"
+        '';
     in
     {
       devShells = forAllSystems (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          lint = lintFor system;
         in
         {
           default = devenv.lib.mkShell {
@@ -58,6 +90,14 @@
             modules = [
               ./devenv.nix
               {
+                packages = [ lint ];
+
+                scripts = {
+                  lt.exec = ''exec lint "$@"'';
+                  fmt.exec = ''exec nix fmt "$@"'';
+                  chk.exec = ''exec nix flake check --impure "$@"'';
+                };
+
                 treefmt = {
                   enable = true;
                   config = treefmtConfig;
@@ -78,14 +118,20 @@
 
       formatter = forAllSystems (system: (treefmtFor system).config.build.wrapper);
 
+      packages = forAllSystems (system: {
+        lint = lintFor system;
+      });
+
       checks = forAllSystems (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           treefmt = treefmtFor system;
+          lint = lintFor system;
         in
         {
           formatting = treefmt.config.build.check self;
+          lint = runCheck system lint;
           mysql-client =
             pkgs.runCommand "isys2014-mysql-client-check" { nativeBuildInputs = [ pkgs.mysql84 ]; }
               ''
